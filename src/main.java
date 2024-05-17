9a48a8563f186379641e63f6ce900cedf769011f
Atma 1 <atmasusanto08@gmail.com>
2024-05-17 20:26:17 +0800

    changed

--- src/main.java
+++ src/main.java
@@ -1,42 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import component_login_register.Login;
+import javax.swing.WindowConstants;
 
-import register.Register;
-
-public class main extends JFrame implements ActionListener {
+public class main {
     
-    JTextField tf = new JTextField();;
     
-    main() {
-        JFrame frame = new JFrame();
-        
-        tf.setBounds(50, 50, 150, 20);
- 
-        JButton button = new JButton(" GFG WebSite Click");
- 
-        button.setBounds(150, 200, 220, 50);
-        
-        button.addActionListener(this);
- 
-        frame.add(tf);
-         
-        frame.add(button);
- 
-        frame.setSize(500, 600);
- 
-        frame.setLayout(null);
- 
-        frame.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
-        new main();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println(tf.getText());
+    public static void main(String args[]) {
+        Login LoginForm = new Login();
+        LoginForm.setVisible(true);
+        LoginForm.pack();
+        LoginForm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     
 }
