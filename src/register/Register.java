0219e5a39dd422e76708607727f8a81871428464
Atma 1 <atmasusanto08@gmail.com>
2024-05-17 17:27:30 +0800

    moved to the right

--- src/register/Register.java
+++ src/register/Register.java
@@ -37,7 +37,6 @@ public class Register extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(898, 418));
         setSize(new java.awt.Dimension(898, 418));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
