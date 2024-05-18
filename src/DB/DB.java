99be129815cda0f05ced262bb31bb0cdaf61f533
Atma 1 <atmasusanto08@gmail.com>
2024-05-18 13:08:12 +0800

    added user and password

--- src/DB/DB.java
+++ src/DB/DB.java
@@ -4,15 +4,21 @@ import java.sql.*;
 
 public class DB {
     private static final String URL = "jdbc:mysql://localhost:3306/spk_bsw";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
     
     public static Connection getConnection() {
         try {
-            Connection con = DriverManager.getConnection(URL);
-            return con;
+            return DriverManager.getConnection(URL, USER,
+                    PASSWORD);
         } catch (SQLException error) {
             System.out.println("Error " + error);
         }
         return null;
+    }
+    
+    public static void addUser(Connection con) {
+        
     }
     
     
