1ca685c6ec51f9ce407c0a4e566d678068d53314
Atma 1 <atmasusanto08@gmail.com>
2024-05-18 13:03:14 +0800

    init

--- src/DB/DB.java
+++ src/DB/DB.java
@@ -1,5 +1,19 @@
 package DB;
 
+import java.sql.*;
+
 public class DB {
+    private static final String URL = "jdbc:mysql://localhost:3306/spk_bsw";
+    
+    public static Connection getConnection() {
+        try {
+            Connection con = DriverManager.getConnection(URL);
+            return con;
+        } catch (SQLException error) {
+            System.out.println("Error " + error);
+        }
+        return null;
+    }
+    
     
 }
